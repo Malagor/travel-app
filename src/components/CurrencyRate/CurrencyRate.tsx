fb5b6cae2dc262f15c2e2b1905d45af722e3f910
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-03-01 10:30:10 +0300

    fix: provide loading and error handling

--- src/components/CurrencyRate/CurrencyRate.tsx
+++ src/components/CurrencyRate/CurrencyRate.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useState, useEffect } from 'react';
 import { CURRENCY_MAP } from 'appConstants/currencyMap';
+import { Loader, ErrorMessage } from 'components';
 import { Paper, List, Select } from '@material-ui/core';
 import classes from './CurrencyRate.module.scss';
 
@@ -10,19 +11,38 @@ type CurrencyRateProps = {
 export const CurrencyRate: FC<CurrencyRateProps> = ({ currentCountry }) => {
   const [currencies, setCurrencies] = useState<string[]>(['USD', 'EUR', 'BYN']);
   const [rates, setRates] = useState<number[]>([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const API_KEY = '4b1c63d88f7f09c5e8b03ab5';
   const baseCurrency = CURRENCY_MAP[currentCountry];
   const apiUrl = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${baseCurrency}`;
 
-  useEffect(() => {
-    const getCurrencyRateData = async () => {
-      const res = await fetch(apiUrl);
-      const data = await res.json();
-      setRates(currencies.map((currency) => data.conversion_rates[currency]));
-    };
+  const onError = () => {
+    setIsError(true);
+    setIsLoading(false);
+  };
 
-    getCurrencyRateData();
+  useEffect(() => {
+    const getCurrencyRateData = () =>
+      fetch(apiUrl)
+        .then((resp) => {
+          if (!resp.ok) {
+            throw new Error('response error');
+          }
+          return resp.json();
+        })
+        .then((data) => {
+          setIsLoading(false);
+          setRates(
+            currencies.map((currency) => data.conversion_rates[currency])
+          );
+          return data;
+        });
+
+    setIsError(false);
+    setIsLoading(true);
+    getCurrencyRateData().catch(onError);
   }, [currentCountry, apiUrl, currencies]);
 
   const rateList = rates.map((rate, index) => (
@@ -51,8 +71,8 @@ export const CurrencyRate: FC<CurrencyRateProps> = ({ currentCountry }) => {
     }
   }
 
-  return (
-    <Paper elevation={3} className={classes.currencyRate}>
+  const drawCurrencyRates = () => (
+    <>
       <h3 className={classes.currencyRate__header}>
         {`1 ${CURRENCY_MAP[currentCountry]} =`}
       </h3>
@@ -64,6 +84,16 @@ export const CurrencyRate: FC<CurrencyRateProps> = ({ currentCountry }) => {
       >
         {currencyOptions}
       </Select>
+    </>
+  );
+
+  const hasData = !(isError || isLoading);
+
+  return (
+    <Paper elevation={3} className={classes.currencyRate}>
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {hasData && drawCurrencyRates()}
     </Paper>
   );
 };
