cde46432e8e04b3d1dc9f97a93d2eb86c491a715
Andrei <denisenko.andrei.da@gmail.com>
2021-03-01 16:07:23 +0300

    ref: change class component to functional

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Clock.module.scss';
 
 const DEGREASE = 6;
+
+type TClockProps = {
+  theme: string;
+};
+
+export const Clock = (props: TClockProps) => {
+  const date = new Date()
+
+  const [hour, setHour] = useState(date.getHours())
+  const [min,setMin] = useState(date.getMinutes())
+  const [sec, setSec] = useState(date.getSeconds())
+
+  const changeSec = () => setSec(sec + DEGREASE)
+  const changeMin = () => setMin(min + DEGREASE)
+  const changeHour = () => setHour(hour + DEGREASE)
+
+  setTimeout(changeSec, 1000)
+  setTimeout(changeMin, 1000 * 60)
+  setTimeout(changeHour, 1000 * 60 * 12)
+
+  const { theme } = props;
+  const cls = [classes.Clock];
+  if (theme === 'light') {
+    cls.push(classes.light);
+  }
+
+  return (
+    <div className={cls.join(' ')}>
+      <div className={classes.hour}>
+        <div
+          className={classes.hr}
+          style={{
+            transform: `rotateZ(${hour + min / 12}deg)`,
+          }}
+        />
+      </div>
+      <div className={classes.min}>
+        <div
+          className={classes.mn}
+          style={{
+            transform: `rotateZ(${min}deg)`,
+          }}
+        />
+      </div>
+      <div className={classes.sec}>
+        <div
+          className={classes.sc}
+          style={{
+            transform: `rotateZ(${sec}deg)`,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+
+
+ 
+
+
+
+
+
+
+
+/* const DEGREASE = 6;
 
 type TClockState = {
   hour: number;
@@ -82,3 +149,4 @@ export class Clock extends React.Component<TClockProps, TClockState> {
     );
   }
 }
+ */
