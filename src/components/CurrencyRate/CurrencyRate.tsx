49da16679fccb44a88c4630d614e8fc7d19d93d8
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-03-01 09:43:18 +0300

    feat: make it possible to choose another currency for reference

--- src/components/CurrencyRate/CurrencyRate.tsx
+++ src/components/CurrencyRate/CurrencyRate.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState, useEffect } from 'react';
 import { CURRENCY_MAP } from 'appConstants/currencyMap';
-import { Paper, List } from '@material-ui/core';
+import { Paper, List, Select } from '@material-ui/core';
 import classes from './CurrencyRate.module.scss';
 
 type CurrencyRateProps = {
@@ -8,7 +8,7 @@ type CurrencyRateProps = {
 };
 
 export const CurrencyRate: FC<CurrencyRateProps> = ({ currentCountry }) => {
-  const [currencies, setCurrencies] = useState(['USD', 'EUR', 'BYN']);
+  const [currencies, setCurrencies] = useState<string[]>(['USD', 'EUR', 'BYN']);
   const [rates, setRates] = useState<number[]>([]);
 
   const API_KEY = '4b1c63d88f7f09c5e8b03ab5';
@@ -31,12 +31,39 @@ export const CurrencyRate: FC<CurrencyRateProps> = ({ currentCountry }) => {
     </li>
   ));
 
+  const currencyOptions = Object.values(CURRENCY_MAP)
+    .filter((currency) => currency !== 'USD' && currency !== 'EUR')
+    .map((currency) => (
+      <option key={currency} value={currency}>
+        {currency}
+      </option>
+    ));
+
+  function handleSelectChange(
+    evt: React.ChangeEvent<{
+      name?: string | undefined;
+      value: unknown;
+    }>
+  ) {
+    const newCurrency = evt.target.value;
+    if (typeof newCurrency === 'string') {
+      setCurrencies(['USD', 'EUR', newCurrency]);
+    }
+  }
+
   return (
     <Paper elevation={3} className={classes.currencyRate}>
       <h3 className={classes.currencyRate__header}>
         {`1 ${CURRENCY_MAP[currentCountry]} =`}
       </h3>
       <List>{rateList}</List>
+      <Select
+        defaultValue={currencies[currencies.length - 1]}
+        native
+        onChange={handleSelectChange}
+      >
+        {currencyOptions}
+      </Select>
     </Paper>
   );
 };
