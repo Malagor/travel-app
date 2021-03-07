58e128de80e17cbe493133be6c0b83d93d694929
Andrei <denisenko.andrei.da@gmail.com>
2021-03-07 16:39:39 +0300

    fix: develop code deleted

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { MainPage, CountryPage } from 'modules';
-import { ClockWidget } from 'components/ClockWidget'; // delete
 import { Layout } from '../Layout/Layout';
 
 
 
 export function App() {
   return (
-    <div>
-      <ClockWidget theme='light'/>
-    </div>
-
-/*     <Layout>
+    <Layout>
        <Switch>
         <Route path="/country/:id" component={CountryPage} />
         <Route path="/country" component={CountryPage} />
         <Route exact path="/" component={MainPage} />
       </Switch>
-    </Layout> */
+    </Layout>
   );
 }
