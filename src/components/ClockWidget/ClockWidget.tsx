fd512dd06d6e08500f5cff202366ad7be019abf2
Andrei <denisenko.andrei.da@gmail.com>
2021-03-10 17:46:51 +0300

    refactor: correction of comments

--- src/components/ClockWidget/ClockWidget.tsx
+++ src/components/ClockWidget/ClockWidget.tsx
@@ -4,9 +4,7 @@ import 'moment/locale/ru';
 import moment from 'moment-timezone';
 import React, {FC, useState, useEffect} from 'react';
 import { useTranslation } from 'react-i18next';
-import { Clock } from './components/Clock';
-import { DigitalWatch } from './components/DigitalWatch';
-import { DateString } from './components/Date';
+import { Clock,  DigitalWatch,  DateString } from './components';
 import classes from './ClockWidget.module.scss';
 
  
@@ -19,7 +17,7 @@ const getTimeForClock = (date: moment.Moment) => {
 }
 
 const getTimeDigitalWatch = (date: moment.Moment) => date.format('hh: mm: ss')
-const getTimeForForDate = (date: moment.Moment) => date.format('dddd, ll')
+const getTimeDate = (date: moment.Moment) => date.format('dddd, ll')
 
 
 type TClockWidgetProps = {
@@ -29,19 +27,18 @@ type TClockWidgetProps = {
 export const ClockWidget: FC<TClockWidgetProps> = ({theme}) => {
   const timezoneMyCity = 'Europe/Minsk'
   const timezoneNewYork = 'America/New_York'
-  const languageNewYork = 'en'
 
   const [, i18n] = useTranslation();
 
   const dateLocaleCity = moment().locale(i18n.language).tz(timezoneMyCity)
-  const dateOtherCity = moment().locale(languageNewYork).tz(timezoneNewYork)
+  const dateOtherCity = moment().locale(i18n.language).tz(timezoneNewYork)
 
   const [localeDate, setLocaleDate] = useState(dateLocaleCity)
   const [otherDate, setOtherDate] = useState(dateOtherCity)
 
   const tick = () => {
     const dateLocaleCityNow = moment().locale(i18n.language).tz(timezoneMyCity)
-    const dateOtherCityNow = moment().locale(languageNewYork).tz(timezoneNewYork)
+    const dateOtherCityNow = moment().locale(i18n.language).tz(timezoneNewYork)
 
     setLocaleDate(dateLocaleCityNow)
     setOtherDate(dateOtherCityNow)
@@ -63,7 +60,7 @@ export const ClockWidget: FC<TClockWidgetProps> = ({theme}) => {
             <DigitalWatch time={getTimeDigitalWatch(localeDate)}  theme={theme}/>
           </div>
           <div className={classes.timeBlock__time}>
-            <DateString day={getTimeForForDate(localeDate)} theme={theme}/>
+            <DateString day={getTimeDate(localeDate)} theme={theme}/>
           </div>
         </div>
 
@@ -74,7 +71,7 @@ export const ClockWidget: FC<TClockWidgetProps> = ({theme}) => {
             <DigitalWatch time={getTimeDigitalWatch(otherDate)} theme={theme}/>
           </div>
           <div className={classes.timeBlock__time}>
-            <DateString day={getTimeForForDate(otherDate)} theme={theme}/>
+            <DateString day={getTimeDate(otherDate)} theme={theme}/>
           </div>
         </div>
 
