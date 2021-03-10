b85a8b8576e0a15454e3791720011149fea007e7
Andrei <denisenko.andrei.da@gmail.com>
2021-03-11 00:04:08 +0300

    fix: changing input data

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
-import { ClockWidget } from 'components/ClockWidget';
 import { Route, Switch } from 'react-router-dom';
 import { MainPage, CountryPage } from 'modules';
 import { Layout } from '../Layout/Layout';
 
-const data = {
-  localeCity: {
-    name: 'Minsk',
-    timezone:'Europe/Minsk',
-    
-  },
-  otherCity: {
-    name: 'New York',
-    timezone: 'America/New_York'
-  }
-}
-
 export function App() {
   return (
-    <ClockWidget data={data} theme='light'/>
-/*     <Layout>
+    <Layout>
       <Switch>
         <Route path="/country/:id" component={CountryPage} />
         <Route path="/country" component={CountryPage} />
         <Route exact path="/" component={MainPage} />
       </Switch>
-    </Layout> */
+    </Layout>
   );
 }
