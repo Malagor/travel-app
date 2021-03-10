b14cedf89862cfd0d3ae7579fdb185b0999a389b
Andrei <denisenko.andrei.da@gmail.com>
2021-03-10 20:05:53 +0300

    fix: fixed props in componetn

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { ClockWidget } from 'components/ClockWidget';
 import { Route, Switch } from 'react-router-dom';
 import { MainPage, CountryPage } from 'modules';
 import { Layout } from '../Layout/Layout';
 
+const data = {
+  localeCity: {
+    name: 'Minsk',
+    timezone:'Europe/Minsk',
+    
+  },
+  otherCity: {
+    name: 'New York',
+    timezone: 'America/New_York'
+  }
+}
+
 export function App() {
   return (
-    <Layout>
+    <ClockWidget data={data} theme='light'/>
+/*     <Layout>
       <Switch>
         <Route path="/country/:id" component={CountryPage} />
         <Route path="/country" component={CountryPage} />
         <Route exact path="/" component={MainPage} />
       </Switch>
-    </Layout>
+    </Layout> */
   );
 }
--- src/components/ClockWidget/ClockWidget.tsx
+++ src/components/ClockWidget/ClockWidget.tsx
@@ -21,37 +21,47 @@ const getCountryDate = (date: moment.Moment) => date.format('dddd, ll')
 
 
 type TClockWidgetProps = {
+  data: {
+    localeCity: {
+      name: string;
+      timezone: string;
+      
+    },
+    otherCity: {
+      name: string;
+      timezone: string;
+    }
+  };
   theme: string;
 };
 
-export const ClockWidget: FC<TClockWidgetProps> = ({theme}) => {
-  const timezoneMyCity = 'Europe/Minsk'
-  const timezoneNewYork = 'America/New_York'
+export const ClockWidget: FC<TClockWidgetProps> = ({data, theme}) => {
+  const { localeCity, otherCity } = data;
 
   const [, i18n] = useTranslation();
 
-  const [localeDate, setLocaleDate] = useState<null | moment.Moment>(null)
-  const [otherDate, setOtherDate] = useState<null | moment.Moment>(null)
+  const [localeDate, setLocaleDate] = useState<null | moment.Moment>(null);
+  const [otherDate, setOtherDate] = useState<null | moment.Moment>(null);
 
   const tick = () => {
-    const dateLocaleCityNow = moment().locale(i18n.language).tz(timezoneMyCity)
-    const dateOtherCityNow = moment().locale(i18n.language).tz(timezoneNewYork)
+    const dateLocaleCityNow = moment().locale(i18n.language).tz(localeCity.timezone);
+    const dateOtherCityNow = moment().locale(i18n.language).tz(otherCity.timezone);
 
-    setLocaleDate(dateLocaleCityNow)
-    setOtherDate(dateOtherCityNow)
+    setLocaleDate(dateLocaleCityNow);
+    setOtherDate(dateOtherCityNow);
   }
 
   useEffect(() => {
-    const timer = setInterval(tick, 1000)
-    return () => clearInterval(timer) 
-  }, [tick])
+    const timer = setInterval(tick, 1000);
+    return () => clearInterval(timer); 
+  }, [tick]);
 
   return (
     <div className={classes.wrapper}>
       <div className={classes.wrapperClock}>
 
         <div className={classes.timeBlock}>
-          <h4 className={theme === 'light' ? classes.lightTheme : classes.darkTheme}>localCity</h4>
+          <h4 className={theme === 'light' ? classes.lightTheme : classes.darkTheme}>{localeCity.name}</h4>
             {localeDate && <Clock time={getTimeForClock(localeDate)} theme={theme}/>}
           <div className={classes.timeBlock__time}>
             {localeDate && <DigitalWatch time={getCountryTime(localeDate)}  theme={theme}/>}
@@ -62,7 +72,7 @@ export const ClockWidget: FC<TClockWidgetProps> = ({theme}) => {
         </div>
 
         <div className={classes.timeBlock}>
-          <h4 className={theme === 'light' ? classes.lightTheme : classes.darkTheme}>{timezoneNewYork}</h4>
+          <h4 className={theme === 'light' ? classes.lightTheme : classes.darkTheme}>{otherCity.name}</h4>
             {otherDate && <Clock time={getTimeForClock(otherDate)} theme={theme}/>}
           <div className={classes.timeBlock__time}>
             {otherDate && <DigitalWatch time={getCountryTime(otherDate)} theme={theme}/>}
