86c3ee497303dc989d9dbc5d452dbf08d5d8a326
Andrei <denisenko.andrei.da@gmail.com>
2021-03-12 01:43:05 +0300

    fix: clear code

--- src/components/Layout/components/Footer/Footer.tsx
+++ src/components/Layout/components/Footer/Footer.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Grid from '@material-ui/core/Grid';
 import ButtonBase from '@material-ui/core/ButtonBase';
-import Button from '@material-ui/core/Button';
-import RSSchool from 'https://rs.school/images/rs_school_js.svg'
 
 const authors = [
   {
