cd6f7b0930677ce44a8b4b13f1e1853154721c04
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-03-12 10:10:36 +0300

    fix: turn the currency rate list into the table

--- src/components/CurrencyRate/components/CurrencyRateView.tsx
+++ src/components/CurrencyRate/components/CurrencyRateView.tsx
@@ -1,5 +1,9 @@
 import React, { FC } from 'react';
-import { List } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import { CURRENCY_MAP } from 'appConstants/currencyMap';
 
 type CurrencyRateViewProps = {
@@ -7,18 +11,39 @@ type CurrencyRateViewProps = {
   rates: [string, number][];
 };
 
+function createData(rate: number, currencyCode: string) {
+  return { rate, currencyCode };
+}
+
+function createRows(ratesList: [string, number][]) {
+  return ratesList.map((rate) => createData(rate[1], rate[0]));
+}
+
 export const CurrencyRateView: FC<CurrencyRateViewProps> = ({
   currentCountry,
   rates,
 }) => {
-  const rateList = rates.map((rate) => (
-    <li key={rate[0]}>{`${rate[1]} ${rate[0]}`}</li>
-  ));
+  const rows = createRows(rates);
 
   return (
-    <>
-      <h3>{`1 ${CURRENCY_MAP[currentCountry]} =`}</h3>
-      <List>{rateList}</List>
-    </>
+    <Table aria-label="currency rate table">
+      <TableHead>
+        <TableRow>
+          <TableCell align="center" colSpan={2}>
+            {`1 ${CURRENCY_MAP[currentCountry]} =`}
+          </TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.currencyCode}>
+            <TableCell component="th" scope="row" align="right">
+              {row.rate}
+            </TableCell>
+            <TableCell>{row.currencyCode}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 };
