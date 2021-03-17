5c8a6b9efe4640c5b6f9ce1f9e040fab801e5673
Liza Veis <liza.veis14@gmail.com>
2021-03-17 03:15:32 +0200

    fix: first video loading

--- src/modules/CountryPage/components/CountryPageView/CountryPageView.tsx
+++ src/modules/CountryPage/components/CountryPageView/CountryPageView.tsx
@@ -61,12 +61,16 @@ export const CountryPageView: FC<CountryPageProps> = ({ country, lang }) => {
                         </Grid>
                       </Grid>
                     </Grid>
-                    <Grid item xs={12} md={7} lg={7}>
-                      <VideoPlayer url={country?.videos?.[0] || ''} />
-                    </Grid>
                   </Grid>
                 </Paper>
               </Grid>
+              {country.videos?.[0] && (
+                <Grid item xs={12}>
+                  <Paper className={classes.paper}>
+                    <VideoPlayer url={country.videos[0] || ''} />
+                  </Paper>
+                </Grid>
+              )}
               {country.attractions && (
                 <Grid item xs={12}>
                   <Paper className={classes.paper}>
