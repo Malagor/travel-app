875f76b742564a082bfc8e5f3fe4eeaba8af53bb
Liza Veis <liza.veis14@gmail.com>
2021-03-17 04:26:10 +0200

    fix: fix weather loading

--- src/appConstants/api.ts
+++ src/appConstants/api.ts
@@ -1,2 +1,2 @@
-export const WEATHER_API_KEY = '0dd4c70fbcb17123e868e6d308f9906a';
+export const WEATHER_API_KEY = 'ba9de510a3e2a4d46ec87d0716c2e01e';
 export const CURRENCY_API_KEY = '4b1c63d88f7f09c5e8b03ab5';
--- src/components/Weather/components/WeatherView.tsx
+++ src/components/Weather/components/WeatherView.tsx
@@ -1,16 +1,16 @@
-import { darken } from '@material-ui/core';
-import { WEATHER_COLOR } from 'appConstants/colors';
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { TWeatherInfo } from 'types';
-import classes from './WeatherView.module.scss';
+import { useStyles } from './styled';
 
 type WeatherViewProps = {
   info: TWeatherInfo;
+  title: string;
 };
 
-export const WeatherView: FC<WeatherViewProps> = ({ info }) => {
-  const { name: cityName } = info;
+export const WeatherView: FC<WeatherViewProps> = ({ info, title }) => {
+  const classes = useStyles();
+
   const { description, icon } = info.weather[0];
   const { temp, humidity } = info.main;
   const { all: cloudCover } = info.clouds;
@@ -21,33 +21,24 @@ export const WeatherView: FC<WeatherViewProps> = ({ info }) => {
 
   return (
     <div className={classes.weather}>
-      <div
-        className={classes.weatherHeader}
-        style={{ backgroundColor: WEATHER_COLOR }}
-      >
-        <div className={classes.weatherTitle}>
-          <h3>{cityName}</h3>
+      <div className={classes.header}>
+        <div className={classes.title}>
+          <h3>{title}</h3>
           <span>{description}</span>
         </div>
-        <div
-          className={classes.weatherIcon}
-          style={{ backgroundImage: iconUrl }}
-        />
+        <div className={classes.icon} style={{ backgroundImage: iconUrl }} />
       </div>
-      <div className={classes.weatherBody}>
-        <div
-          className={classes.weatherTemperature}
-          style={{ color: darken(WEATHER_COLOR, 0.35) }}
-        >{`${Math.round(temp)}°`}</div>
-        <div className={classes.weatherInfo}>
-          <div className={classes.weatherItem}>
+      <div className={classes.body}>
+        <div className={classes.temperature}>{`${Math.round(temp)}°`}</div>
+        <div className={classes.info}>
+          <div className={classes.item}>
             {t('Weather.humidity')}:<span>{`${humidity}%`}</span>
           </div>
-          <div className={classes.weatherItem}>
+          <div className={classes.item}>
             {t('Weather.wind')}:
             <span>{`${windSpeed} ${t('Weather.metersPerSecond')}`}</span>
           </div>
-          <div className={classes.weatherItem}>
+          <div className={classes.item}>
             {t('Weather.cloudCover')}:<span>{`${cloudCover}%`}</span>
           </div>
         </div>
